9fa7c68f429ca9031a113b5265505c0c04cfc482
Stephanie <s.brandt@cern.ch>
2016-08-09 15:45:08 +0200

    Test new branch

--- RecoilCorrections/runRecoilFits.sh
+++ RecoilCorrections/runRecoilFits.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
-INPUTDIR=/data/blue/sabrandt/Bacon/wz_flat_2015D
-
+INPUTDIR=/afs/cern.ch/user/s/sabrandt/work/public/SM/newBacon
+LUMI=2318
 
 # muons
  #use fitRecoilZmm.C+(inputFile, nSigma for U1 fits, nSigma for U2 fits, 1, name of u1 variable, name of u2 variable, name appended to output file, output folder)
- root -l -q fitRecoil.C+\(\"${INPUTDIR}/Zmumu/ntuples/zmm_select.raw.root\",2,2,1,\"u1\",\"u2\",\"pf\",\"Zmm_test\"\) 
+# root -l -q fitRecoil.C+\(\"${INPUTDIR}/Zmumu/ntuples/zmm_select.raw.root\",2,2,1,\"u1\",\"u2\",\"pf\",\"Zmm_test\"\) 
+
+ # for data, you should use 1 as the lumi (or remove the LUMI argument)
+ #root -l -q fitRecoilPuppi.C+\(\"${INPUTDIR}/Zmumu/ntuples/zmm_select.raw.root\",2,2,1,\"puppiU1\",\"puppiU2\",\"puppi\",\"Zmm_test\",${LUMI}\) 
+ root -l -q fitRecoilPuppi.C+\(\"${INPUTDIR}/Zmumu/ntuples/data_select.root\",2,2,1,\"puppiU1\",\"puppiU2\",\"puppi\",\"Zmm_data_test\"\) 
 
 rm *.so *.d *.pcm
